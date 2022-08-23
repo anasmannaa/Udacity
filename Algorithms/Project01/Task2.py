8e706f1444105046d14fe4fc730452aefb272ced
Anas Mannaa <30526159+anasmannaa@users.noreply.github.com>
2022-08-23 11:40:04 -0500

    Update Task2.py

--- Algorithms/Project01/Task2.py
+++ Algorithms/Project01/Task2.py
@@ -32,13 +32,27 @@ print result including list[index][0] as the phone number required
 """
 
 def longest_call(re_list):
-    index = 0
-    max_duration = 0
-    for i in range(len(re_list) - 1):
-        if int(re_list[i][3]) > max_duration:
-            index = i
-            max_duration = int(re_list[i][3])
-            
-    print (re_list[index][0], "spent the longest time, ", re_list[index][3], "seconds, on the phone during September 2016.")
+    calls_map = {}
+    for call in re_list:
+        caller = call[0]
+        reciever = call[1]
+        duration = int(call[3])
+        if caller in calls_map:
+            calls_map[caller] += duration
+        else:
+            calls_map[caller] = duration
 
+        if reciever in calls_map:
+            calls_map[reciever] += duration
+        else:
+            calls_map[reciever] = duration
+
+    longest_duration = 0
+    phone_number = ""
+    for key in calls_map:
+        if calls_map[key] > longest_duration:
+            longest_duration = calls_map[key]
+            phone_number = key
+    print (phone_number, "spent the longest time, ", longest_duration, "seconds, on the phone during September 2016.")
+        
 longest_call(calls)
